9f03d15b3f7f5e7a557fda40232c4c980011e6db
Maximilian Hauser <info@hauser-media.com>
2024-03-11 22:03:29 +0100

    Added Kategorien

--- craigslist-redesign/src/App.js
+++ craigslist-redesign/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/header';
 import Hero from './components/hero';
+import Categories from './components/categories';
 
 function App() {
   return (
     <div className="App">
       {/* <Header /> */}
       <Hero />
+      <Categories />
     </div>
   );
 }
