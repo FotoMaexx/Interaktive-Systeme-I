99b6aef9dd1b075cfa11c2e5d6ab6b131352161e
Maximilian Hauser <info@hauser-media.com>
2024-03-11 22:20:25 +0100

    Added Stats

--- craigslist-redesign/src/App.js
+++ craigslist-redesign/src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/hero';
 import Categories from './components/categories';
 import ProductSlider from './components/homepage-product-slider';
 import { FooterWithSocialLinks as Footer } from './components/footer';
+import Stats from './components/homepage-stats';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Categories />
       <ProductSlider />
+      <Stats />
       <Footer />
     </div>
   );
