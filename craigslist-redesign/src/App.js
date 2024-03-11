2300ea0a93d9d81785a3fa997e07c0249ce6b8f8
Maximilian Hauser <info@hauser-media.com>
2024-03-11 22:06:46 +0100

    Added #4

--- craigslist-redesign/src/App.js
+++ craigslist-redesign/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header';
 import Hero from './components/hero';
 import Categories from './components/categories';
+import ProductSlider from './components/homepage-product-slider';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       {/* <Header /> */}
       <Hero />
       <Categories />
+      <ProductSlider />
     </div>
   );
 }
