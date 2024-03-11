14b582dc43c4749cbaae38373f1924fd8c7fc572
Maximilian Hauser <info@hauser-media.com>
2024-03-11 21:55:56 +0100

    Added #3
    neues Problem: #2

--- craigslist-redesign/src/App.js
+++ craigslist-redesign/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/header';
+import Hero from './components/hero';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
+      <Hero />
     </div>
   );
 }
