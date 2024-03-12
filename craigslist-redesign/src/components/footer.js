1605437c62b6db99fa0c119d142964a3f2c4488b
Maximilian Hauser <info@hauser-media.com>
2024-03-12 21:31:30 +0100

    fixed #2 fixed #5

--- craigslist-redesign/src/components/footer.js
+++ craigslist-redesign/src/components/footer.js
@@ -19,7 +19,7 @@ const currentYear = new Date().getFullYear();
  
 export function FooterWithSocialLinks() {
   return (
-    <footer className="relative w-full">
+    <footer className="relative w-full mt-24">
       <div className="mx-auto w-full max-w-7xl px-8">
         <div className="grid grid-cols-1 justify-between gap-4 md:grid-cols-2">
           <Typography variant="h5" className="mb-6">
