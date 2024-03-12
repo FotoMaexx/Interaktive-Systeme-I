88545933183d9a3cf345bf808b8cf1383cf34aba
Maximilian Hauser <info@hauser-media.com>
2024-03-12 22:14:42 +0100

    Updated Product Grid

--- craigslist-redesign/src/components/product-grid.js
+++ craigslist-redesign/src/components/product-grid.js
@@ -1,17 +1,3 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
 const products = [
     {
       id: 1,
@@ -28,9 +14,7 @@ const products = [
   export default function Example() {
     return (
       <div className="bg-white">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
-  
+        <div className="mx-auto max-w-2xl px-4 lg:max-w-7xl">
           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {products.map((product) => (
               <div key={product.id} className="group relative">
