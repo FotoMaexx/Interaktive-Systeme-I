a33fc7f35c74186f5238ab9bafe99c2613da2c3c
Maximilian Hauser <info@hauser-media.com>
2024-03-12 22:12:58 +0100

    Added Filterpage to search

--- craigslist-redesign/src/pages/Suche.js
+++ craigslist-redesign/src/pages/Suche.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import Header from '../components/header';
-import Categories from '../components/categories';
-import ProductSlider from '../components/homepage-product-slider';
+import FilterPage from '../components/filterpage';
 import { FooterWithSocialLinks as Footer } from '../components/footer';
-import Stats from '../components/homepage-stats';
 
 function Home() {
   return (
     <div className="App">
       <Header />
-      {/* <Hero /> */}
-      <Categories />
-      <ProductSlider />
-      <Stats />
+      <FilterPage />
       <Footer />
     </div>
   );
