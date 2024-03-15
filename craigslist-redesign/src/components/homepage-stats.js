67ce1a8561cca1bddca14ec3201b5ac120c964f8
Maximilian Hauser <info@hauser-media.com>
2024-03-15 10:13:23 +0100

    Stats überarbeitet

--- craigslist-redesign/src/components/homepage-stats.js
+++ craigslist-redesign/src/components/homepage-stats.js
@@ -1,7 +1,7 @@
 const stats = [
-    { id: 1, name: 'Transactions every 24 hours', value: '44 million' },
-    { id: 2, name: 'Assets under holding', value: '$119 trillion' },
-    { id: 3, name: 'New users annually', value: '46,000' },
+    { id: 1, name: 'Artikel Online', value: '300.000' },
+    { id: 2, name: 'Verkäufe bisher', value: '3.5 Mio' },
+    { id: 3, name: 'Neue User täglich', value: '50 Tsd.' },
   ]
   
   export default function Example() {
