820577f491fff998bc38d0e727d99e98da3d790b
Maximilian Hauser <info@hauser-media.com>
2024-03-15 11:33:25 +0100

    Suchleiste zu Produktseite hinzugefügt

--- craigslist-redesign/src/pages/Product.js
+++ craigslist-redesign/src/pages/Product.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from '../components/header';
 import { FooterWithSocialLinks as Footer } from '../components/footer';
 import ProductOVerview from '../components/product';
+import SearchBar from '../components/search-bar';
 
 function Product() {
     return (
         <div className="App">
             <Header />
+            <SearchBar />
             <ProductOVerview />
             <Footer />
         </div>
