50d0c3c97204d4e66698c89a5263b4bfa651d423
Maximilian Hauser <info@hauser-media.com>
2024-03-15 10:56:36 +0100

    Hinzufügen der Suchleiste

--- craigslist-redesign/src/pages/Suche.js
+++ craigslist-redesign/src/pages/Suche.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/header';
 import FilterPage from '../components/filterpage';
+import SearchBar from '../components/search-bar';
 import { FooterWithSocialLinks as Footer } from '../components/footer';
 
 function Suche() {
   return (
     <div className="App">
       <Header />
+      <SearchBar />
       <FilterPage />
       <Footer />
     </div>
