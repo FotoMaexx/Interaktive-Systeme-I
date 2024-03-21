2cba0b1dc14560ee04c70bf4b52ddf50131d63b2
PiaKatharina <schwarz.pia26@gmail.com>
2024-03-21 20:19:56 +0100

    update home
    style

--- craigslist-redesign/src/components/header.js
+++ craigslist-redesign/src/components/header.js
@@ -17,6 +17,8 @@ import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/
 import logo from '../assets/img/logo.svg';
 import { Link } from 'react-router-dom';
 
+
+
 // Importiere das Heroicons-Icon für das Login-Symbol
 import { UserIcon } from '@heroicons/react/20/solid'; //login
 
@@ -51,6 +53,11 @@ const community = [
   const jobs = [
     { name: 'Karriere & Beruf', description: 'Finde deinen Traumjob oder neue berufliche Herausforderungen.', href: '#', icon: BriefcaseIcon },
     { name: 'Nebenjobs & Freelancing', description: 'Flexibel arbeiten mit Nebenjobs und Freelancing-Aufträgen.', href: '#', icon: CurrencyDollarIcon },
+  ];
+
+  const log = [
+    { name: 'Anmelden', description: 'Du hast bereits ein Konto.', href: '#', icon: BriefcaseIcon },
+    { name: 'Registrieren', description: 'Du bist neu hier.', href: '#', icon: CurrencyDollarIcon },
   ];
 
 
@@ -300,6 +307,46 @@ export default function Example() {
               <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
                 <div className="p-4">
                   {jobs.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                      </div>
+                      <div className="flex-auto">
+                        <a href={item.href} className="block font-semibold text-gray-900">
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
+            <UserIcon className="h-5 w-5 mr-1" aria-hidden="true" />
+              Login
+             
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {log.map((item) => (
                     <div
                       key={item.name}
                       className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
