e7a205aa385a90b1e212b5e248991932846543e6
Maximilian Hauser <info@hauser-media.com>
2024-04-05 08:51:21 +0200

    Added Search Radius

--- craigslist-redesign/src/components/search-bar.js
+++ craigslist-redesign/src/components/search-bar.js
@@ -5,7 +5,8 @@ import { useNavigate, useLocation } from 'react-router-dom';
 export default function Example() {
   const [searchTerm, setSearchTerm] = useState('');
   const [location, setLocation] = useState('');
-  const searchRef = useRef(null); // Referenz für den übergeordneten Container
+  const [radius, setRadius] = useState('');
+  const searchRef = useRef(null);
   const navigate = useNavigate();
   const locationHook = useLocation();
 
@@ -15,11 +16,12 @@ export default function Example() {
     if (state) {
       setSearchTerm(state.searchTerm || '');
       setLocation(state.location || '');
+      setRadius(state.radius || '');
     }
   }, [locationHook]);
 
   const handleSearchClick = () => {
-    navigate('/suche', { state: { searchTerm, location } });
+    navigate('/suche', { state: { searchTerm, location, radius } });
   };
 
   const handleFocus = () => {
@@ -32,14 +34,14 @@ export default function Example() {
 
   return (
     <div className="max-w-7xl px-4 sm:px-6 lg:px-8 justify-center mx-auto mt-2">
-      <div 
+      <div
         ref={searchRef}
         className="relative rounded-md shadow-sm flex items-center border border-gray-300"
         onFocus={handleFocus}
         onBlur={handleBlur}
         tabIndex="-1"
       >
-        <span className="text-gray-500 sm:text-sm pl-3">Was?:</span>
+        <span className="text-gray-900 font-bold sm:text-sm px-3">Was?:</span>
         <input
           type="text"
           value={searchTerm}
@@ -50,7 +52,7 @@ export default function Example() {
           onBlur={handleBlur}
         />
         <span className="border-l border-gray-300 h-6"></span>
-        <span className="text-gray-500 sm:text-sm px-3">Wo?:</span>
+        <span className="text-gray-900 font-bold sm:text-sm px-3">Wo?:</span>
         <input
           type="text"
           value={location}
@@ -60,8 +62,22 @@ export default function Example() {
           onFocus={handleFocus}
           onBlur={handleBlur}
         />
-        <button 
-          onClick={handleSearchClick} 
+        {/* Dropdown für den Umkreis */}
+        <select
+          value={radius}
+          onChange={(e) => setRadius(e.target.value)}
+          className="ml-2 py-2 text-gray-700 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-none focus:outline-none focus:ring-0 rounded-md"
+        >
+          <option value="">Umkreis wählen</option>
+          <option value="5">5km</option>
+          <option value="10">10km</option>
+          <option value="20">20km</option>
+          <option value="30">30km</option>
+          <option value="50">50km</option>
+          <option value="100">100km</option>
+        </select>
+        <button
+          onClick={handleSearchClick}
           className="p-2"
           aria-label="Suche">
           <MagnifyingGlassIcon className="h-5 w-5 text-gray-500 hover:text-gray-700" />
