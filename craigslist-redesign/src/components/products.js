936045c7c10530c1445ced7abf5968ecaa4f196f
Maximilian Hauser <info@hauser-media.com>
2024-04-17 19:04:05 +0200

    New ProductPage Design

--- craigslist-redesign/src/components/products.js
+++ craigslist-redesign/src/components/products.js
@@ -1,9 +1,9 @@
 export const products = [
   {
     id: 1,
-    name: 'Basic Tee 6-Pack',
-    href: '/product/1',
-    price: '192€',
+    name: 'Name',
+    href: '/product',
+    price: '1111',
     images: [
       {
         src: 'https://example.com/img/products/1_1.jpg',
@@ -14,30 +14,14 @@ export const products = [
       {
         src: 'https://example.com/img/products/1_3.jpg',
       },
-      {
-        src: 'https://example.com/img/products/1_4.jpg',
-      },
     ],
-    colors: [
-      { name: 'Black', class: 'bg-gray-900', selectedClass: 'ring-gray-900' },
-    ],
-    sizes: [
-      { name: 'XXS', inStock: false },
-      { name: 'XS', inStock: true },
-      { name: 'S', inStock: true },
-      { name: 'M', inStock: true },
-      { name: 'L', inStock: true },
-      { name: 'XL', inStock: true },
-      { name: '2XL', inStock: true },
-      { name: '3XL', inStock: true },
-    ],
-    description: 'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
+    description: 'Beschri',
     highlights: [
-      'Hand cut and sewn locally',
-      'Dyed with our proprietary colors',
-      'Pre-washed & pre-shrunk',
-      'Ultra-soft 100% cotton',
+      'High 1',
+      'High 2',
+      'High 3',
+      'High 4',
     ],
-    details: 'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
+    details: 'Detis',
   },
 ];
