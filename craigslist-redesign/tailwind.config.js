dd86f53f72cb12a9f3c6dd49173e59e01917d2bf
Maximilian Hauser <info@hauser-media.com>
2024-04-17 11:38:55 +0200

    Added Color Scheme
    #31

--- craigslist-redesign/tailwind.config.js
+++ craigslist-redesign/tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
         '[auto,auto,1fr]': 'auto auto 1fr',
       },
     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'cl1': '#9C4DCC',
+      'cl2': '#38006B',
+      'cl3': '#FFFFFF',
+      'cl4': '#8D8D8D',
+      'cl5': '#000000',
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
