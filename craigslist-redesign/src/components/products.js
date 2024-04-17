597f53500edbcc329da5d6a4c343ca5ff841eae0
Maximilian Hauser <info@hauser-media.com>
2024-04-17 20:16:29 +0200

    Filled Product List
    #10

--- craigslist-redesign/src/components/products.js
+++ craigslist-redesign/src/components/products.js
@@ -1,27 +1,382 @@
+import aa from '../assets/img/products/1/1.webp';
+import ab from '../assets/img/products/1/2.webp';
+import ac from '../assets/img/products/1/3.webp';
+import ba from '../assets/img/products/2/1.webp';
+import bb from '../assets/img/products/2/2.webp';
+import bc from '../assets/img/products/2/3.webp';
+import ca from '../assets/img/products/3/1.webp';
+import cb from '../assets/img/products/3/2.webp';
+import cc from '../assets/img/products/3/3.webp';
+import da from '../assets/img/products/4/1.webp';
+import db from '../assets/img/products/4/2.webp';
+import dc from '../assets/img/products/4/3.webp';
+import ea from '../assets/img/products/5/1.webp';
+import eb from '../assets/img/products/5/2.webp';
+import ec from '../assets/img/products/5/3.webp';
+import fa from '../assets/img/products/6/1.webp';
+import fb from '../assets/img/products/6/2.webp';
+import fc from '../assets/img/products/6/3.webp';
+import ga from '../assets/img/products/7/1.webp';
+import gb from '../assets/img/products/7/2.webp';
+import gc from '../assets/img/products/7/3.webp';
+import ha from '../assets/img/products/8/1.webp';
+import hb from '../assets/img/products/8/2.webp';
+import hc from '../assets/img/products/8/3.webp';
+import ia from '../assets/img/products/9/1.webp';
+import ib from '../assets/img/products/9/2.webp';
+import ic from '../assets/img/products/9/3.webp';
+import ja from '../assets/img/products/10/1.webp';
+import jb from '../assets/img/products/10/2.webp';
+import jc from '../assets/img/products/10/3.webp';
+import ka from '../assets/img/products/11/1.webp';
+import kb from '../assets/img/products/11/2.webp';
+import kc from '../assets/img/products/11/3.webp';
+import la from '../assets/img/products/12/1.webp';
+import lb from '../assets/img/products/12/2.webp';
+import lc from '../assets/img/products/12/3.webp';
+
 export const products = [
   {
     id: 1,
-    name: 'Name',
+    name: 'SIGNE T-SHIRT - WHITE',
     href: '/product',
-    price: '1111',
+    price: '54,90€',
     images: [
       {
-        src: 'https://example.com/img/products/1_1.jpg',
+        src: aa,
       },
       {
-        src: 'https://example.com/img/products/1_2.jpg',
+        src: ab,
       },
       {
-        src: 'https://example.com/img/products/1_3.jpg',
+        src: ac,
       },
     ],
-    description: 'Beschri',
+    description: 'The SIGNE T-SHIRT, with a proven cropped and boxy fit is crafted from incredibly soft 295gsm 100% premium cotton. Featuring the SIGNE embroidered on the chest, symbolizing the beating heart of street culture found within the vibrant streets of Paris.',
     highlights: [
-      'High 1',
-      'High 2',
-      'High 3',
-      'High 4',
+      '295gsm 100 % premium cotton',
+      'SIGNE embroidery on the chest ',
+      'handmade in a european atelier',
+      'proven cropped and boxy fit',
+      'flag label for authenticity',
     ],
-    details: 'Detis',
+    details: 'The model is 184 cm tall & wears a size L',
+    color: 'White',
+    size: 'XS',
+    category: 'abholung'
   },
+  {
+    id: 2,
+    name: 'SIGNE T-SHIRT - BLACK',
+    href: '/product',
+    price: '54,90€',
+    images: [
+      {
+        src: ba,
+      },
+      {
+        src: bb,
+      },
+      {
+        src: bc,
+      },
+    ],
+    description: 'The SIGNE T-SHIRT, with a proven cropped and boxy fit is crafted from incredibly soft 295gsm 100% premium cotton. Featuring the SIGNE embroidered on the chest, symbolizing the beating heart of street culture found within the vibrant streets of Paris.',
+    highlights: [
+      '295gsm 100% premium cotton',
+      'SIGNE embroidery on the chest ',
+      'handmade in a european atelier',
+      'proven cropped and boxy fit',
+      'flag label for authenticity',
+    ],
+    details: 'The model is 184 cm tall & wears a size L',
+    color: 'Black',
+    size: 'S',
+    category: 'versand'
+  },
+  {
+    id: 3,
+    name: 'CREATIVE DEPT T-SHIRT - FADED GREY',
+    href: '/product',
+    price: '64,90€',
+    images: [
+      {
+        src: ca,
+      },
+      {
+        src: cb,
+      },
+      {
+        src: cc,
+      },
+    ],
+    description: 'The CREATIVE DEPT T-SHIRT, featuring a handmade fade grey wash effect on the body further enhance its individuality, making each piece truly unique. Additionally, an white print on the chest & back as well as handmade destroyed effects adds a touch of sophistication, made for Creative Department members.',
+    highlights: [
+      'handmade faded grey',
+      'white CD print on the front & back',
+      'handmade destroyed effects',
+      '295gsm premium cotton',
+      'proven cropped and boxy fit',
+      'handmade in Europe',
+    ],
+    details: 'The model is 184 cm tall & wears a size L',
+    color: 'Gray',
+    size: 'M',
+    category: 'kaeuferschutz'
+  },
+  {
+    id: 4,
+    name: 'SIGNE BUTTON SHIRT - BLACK',
+    href: '/product',
+    price: '99,90€',
+    images: [
+      {
+        src: da,
+      },
+      {
+        src: db,
+      },
+      {
+        src: dc,
+      },
+    ],
+    description: 'The SIGNE BUTTON SHIRT, with a proven cropped and boxy fit is crafted from incredibly soft peached black canvas. Featuring the SIGNE pockets on the chest, and the metal SIGNE, symbolizing the beating heart of street culture found within the vibrant streets of Paris.',
+    highlights: [
+      'SIGNE pockets on the chest',
+      'metal SIGNE',
+      'handmade in a european atelier',
+      'proven cropped and boxy fit',
+      'flag label for authenticity',
+    ],
+    details: 'The model is 184 cm tall & wears a size L',
+    color: 'Black',
+    size: 'L',
+    category: 'paypal'
+  },
+  {
+    id: 5,
+    name: 'VINTAGE OVERSHIRT - BLACK',
+    href: '/product',
+    price: '139,90€',
+    images: [
+      {
+        src: ea,
+      },
+      {
+        src: eb,
+      },
+      {
+        src: ec,
+      },
+    ],
+    description: 'The vintage overshirt, features a proven boxy fit, and decorative seams and pockets in signe form. The famous RETERNITY metal logo on the chest, and handmade destroyed effects completes the product.',
+    highlights: [
+      'vintage washed and destroyed effects',
+      'RETERNITY metal logo',
+      'proven cropped and boxy fit',
+      'handmade in Europe',
+    ],
+    details: 'The model is 186 cm tall & wears a size L',
+    color: 'Black',
+    size: 'XL',
+    category: 'abholung'
+  },
+  {
+    id: 6,
+    name: 'EMBROIDERED T-SHIRT - FADED BLUE-GREY',
+    href: '/product',
+    price: '74,90€',
+    images: [
+      {
+        src: fa,
+      },
+      {
+        src: fb,
+      },
+      {
+        src: fc,
+      },
+    ],
+    description: 'The T-Shirt features a proven cropped and boxy fit and is enhanced by a handmade faded acid wash in a unique blue-grey hue that renders each piece truly one-of-a-kind. The meticulous RETERNITY tonal embroidery on the chest adds an intricate touch to this exceptional piece.',
+    highlights: [
+      'Handmade blue-grey faded acid wash',
+      'tonal embroidery on the front',
+      'Flag label for authenticity',
+      '295gsm premium cotton',
+      'Proven cropped and boxy fit',
+      'handmade in Europe',
+    ],
+    details: 'The model is 184 cm tall & wears a size L',
+    color: 'blue',
+    size: '2XL',
+    category: 'versand'
+  },
+  {
+    id: 7,
+    name: 'LONGSLEEVE BEYOND VISION - BLACK/GREY',
+    href: '/product',
+    price: '89,90€',
+    images: [
+      {
+        src: ga,
+      },
+      {
+        src: gb,
+      },
+      {
+        src: gc,
+      },
+    ],
+    description: '100% premium Baumwolle - 295gsm. Made in Europe',
+    highlights: [
+      'Dropped Shoulder Fit',
+      'Siebdruck auf der Vorderseite und Ärmel',
+      'Kordelstick auf der Rückseite',
+      'Leichte destroyed Effekte am Kragen, Ärmel und Saum',
+      'Flaglabel',
+      'Acid washed',
+    ],
+    details: 'Das Model ist 184cm groß & trägt Größe L',
+    color: 'Black',
+    size: 'XS',
+    category: 'kaeuferschutz'
+  },
+  {
+    id: 8,
+    name: 'T-SHIRT - WASHED BLACK/WASHED GREY',
+    href: '/product',
+    price: '74,90€',
+    images: [
+      {
+        src: ha,
+      },
+      {
+        src: hb,
+      },
+      {
+        src: hc,
+      },
+    ],
+    description: '100% premium Baumwolle - 295gsm. Made in Europe',
+    highlights: [
+      'Dropped Shoulder Fit',
+      'Siebdruck auf der Vorderseite',
+      'Acid washed',
+      'Flaglabel',
+      'Destroyed Effekt am Kragen',
+      'Offene Naht an Ärmel und Saum',
+    ],
+    details: 'Das Model ist 183cm groß & trägt Größe L',
+    color: 'Gray',
+    size: 'S',
+    category: 'paypal'
+  },
+  {
+    id: 9,
+    name: 'CREATIVE DEPT T-SHIRT - CREAM',
+    href: '/product',
+    price: '64,90€',
+    images: [
+      {
+        src: ia,
+      },
+      {
+        src: ib,
+      },
+      {
+        src: ic,
+      },
+    ],
+    description: '100% premium Baumwolle - 295gsm. Made in Europe',
+    highlights: [
+      'Dropped Shoulder Fit',
+      'Puffprint auf der Vorder- und Rückseite',
+      'Flaglabel',
+    ],
+    details: 'Das Model ist 184cm groß & trägt Größe L',
+    color: 'Beige',
+    size: 'M',
+    category: 'abholung'
+  },
+  {
+    id: 10,
+    name: 'RAGLAN T-SHIRT ATELIER 18 - WHITE/BLACK',
+    href: '/product',
+    price: '74,90€',
+    images: [
+      {
+        src: ja,
+      },
+      {
+        src: jb,
+      },
+      {
+        src: jc,
+      },
+    ],
+    description: '100% premium Baumwolle - 295gsm. Made in Europe',
+    highlights: [
+      'Raglanärmel',
+      'Puffprint auf der Vorderseite',
+      'Flaglabel',
+    ],
+    details: 'Das Model ist 184cm groß & trägt Größe L',
+    color: 'White',
+    size: 'L',
+    category: 'versand'
+  },
+  {
+    id: 11,
+    name: 'T-SHIRT \'AIN\'T HERE FOR LOVE\' - CREAM/BLUE',
+    href: '/product',
+    price: '74,90€',
+    images: [
+      {
+        src: ka,
+      },
+      {
+        src: kb,
+      },
+      {
+        src: kc,
+      },
+    ],
+    description: '100% premium Baumwolle - 250gsm. Made in Europe',
+    highlights: [
+      'Dropped Shoulder Fit',
+      'Puffprint auf der Vorderseite',
+      'Bündchen in Kontrastfarbe',
+    ],
+    details: 'Das Model ist 184cm groß & trägt Größe L',
+    color: 'Beige',
+    size: 'XL',
+    category: 'kaeuferschutz'
+  },
+  {
+    id: 12,
+    name: 'T-SHIRT \'GALLERY OF DREAMS\' - VINTAGE GREEN',
+    href: '/product',
+    price: '74,90€',
+    images: [
+      {
+        src: la,
+      },
+      {
+        src: lb,
+      },
+      {
+        src: lc,
+      },
+    ],
+    description: '100% premium Baumwolle - 250gsm. Made in Europe',
+    highlights: [
+      'Dropped Shoulder Fit',
+      'Puffprint auf der Vorderseite',
+      'Flaglabel',
+    ],
+    details: 'Das Model ist 184cm groß & trägt Größe L',
+    color: 'Black',
+    size: '2XL',
+    category: 'paypal'
+  }
 ];
