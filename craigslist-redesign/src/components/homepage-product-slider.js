7f2545f0a3a50fe9d8c33b85b525753301ae892f
Maximilian Hauser <info@hauser-media.com>
2024-04-17 18:51:53 +0200

    Updated Slider to new Array Design

--- craigslist-redesign/src/components/homepage-product-slider.js
+++ craigslist-redesign/src/components/homepage-product-slider.js
@@ -17,7 +17,7 @@ export default function HomepageProductSlider() {
           {randomProducts.map((product) => (
             <div key={product.id} className="group relative">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                <img src={product.imageSrc} alt={product.imageAlt} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
+                <img src={product.images[0].src} className="h-full w-full object-cover object-center lg:h-full lg:w-full" />
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
