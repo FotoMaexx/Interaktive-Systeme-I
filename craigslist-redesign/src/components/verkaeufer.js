5422fd95a97ea47c0e350166fd4e55c4b7921468
PiaKatharina <schwarz.pia26@gmail.com>
2024-04-18 11:34:23 +0200

    Update verkaeufer.js

--- craigslist-redesign/src/components/verkaeufer.js
+++ craigslist-redesign/src/components/verkaeufer.js
@@ -2,25 +2,26 @@ import React from 'react';
 
 export default function Verkaeufer() {
   return (
-    <div class="bg-white py-24 sm:py-32">
-      <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-        <div class="max-w-2xl">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Verkäufer</h2>
-          
-        </div>
-        <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-          <li>
-            <div class="flex items-center gap-x-6">
-              <img class="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              <div>
-                <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Leslie Alexander</h3>
-                <p class="text-sm font-semibold leading-6 text-purple-600">Co-Founder / CEO</p>
-                <p class="text-sm font-semibold leading-6 text-purple-600">leslie.amelie@gmail.com</p>
+    <div className="bg-white py-12 sm:py-16">
+      <div className="mx-auto max-w-7xl grid gap-8 px-6 lg:px-8 xl:grid-cols-3">
+        <div className="max-w-2xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Verkäufer</h2>
+          <ul role="list" className="mt-8">
+            <li>
+              <div className="flex items-center gap-6">
+                <img className="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
+                <div>
+                  <h3 className="text-base font-semibold leading-6 text-gray-900">Leslie Alexander</h3>
+                  <p className="text-sm font-semibold leading-5 text-purple-600">Co-Founder / CEO</p>
+                  <p className="text-sm font-semibold leading-5 text-purple-600">leslie.amelie@gmail.com</p>
+                </div>
               </div>
-            </div>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
 }
+
+
